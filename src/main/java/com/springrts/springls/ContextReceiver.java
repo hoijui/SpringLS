bc24ea41e2cbaec0610ec423af9c1237f3f902ad
hoijui <hoijui.quaero@gmail.com>
2014-12-25 17:39:45 +0100

    add a FIXME comment [minor]

--- src/main/java/com/springrts/springls/ContextReceiver.java
+++ src/main/java/com/springrts/springls/ContextReceiver.java
@@ -22,5 +22,5 @@ package com.springrts.springls;
  */
 public interface ContextReceiver {
 
-	void receiveContext(Context context);
+	void receiveContext(Context context); // FIXME rename to setContext
 }
